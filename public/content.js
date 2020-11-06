9d385f392064728a9c4e7c2c98d7e1a518c3129b
Joel Field <joelmatthewfield@outlook.com>
2020-11-07 09:20:40 +1000

    improve image filtering

--- public/content.js
+++ public/content.js
@@ -1,6 +1,41 @@
 chrome.runtime.onMessage.addListener((message, sender, sendRes) => {
   const imgs = [];
-  const imgElements = document.querySelectorAll("img");
-  imgElements.forEach((img) => imgs.push(img.src));
-  sendRes(imgs);
+  const imgElements = document.querySelectorAll("body img");
+  const keyWordsToIgnore = [
+    "zipmoney",
+    "afterpay",
+    "logo",
+    "header",
+    "footer",
+    "menu",
+    "chat",
+    "location",
+    "facebook",
+    "twitter",
+    "instagram",
+    "youtube",
+    "App store",
+    "Google play",
+    "Visa",
+    "Amex",
+    "Paypal",
+    "Mastercard",
+  ];
+  imgElements.forEach((img) => imgs.push(img));
+  const filteredImages = imgs
+    .filter((img) => {
+      return (
+        img.src &&
+        img.src.length &&
+        !keyWordsToIgnore.find((keyWord) => {
+          return (
+            (img.alt &&
+              img.alt.toLowerCase().includes(keyWord.toLowerCase())) ||
+            (img.classList && img.classList.contains(keyWord.toLowerCase()))
+          );
+        })
+      );
+    })
+    .map((img) => img.src);
+  sendRes(filteredImages);
 });
