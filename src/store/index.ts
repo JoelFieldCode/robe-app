ce8d32535086f20429a741c26f262393eb1574d7
Joel Field <joelmatthewfield@outlook.com>
2021-06-02 21:00:48 +1000

    remove redux

--- src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./createReducer";
-
-const store = configureStore({ reducer: rootReducer });
-
-export default store;
-export type AppDispatch = typeof store.dispatch;
