de331f0ab2bfa151452c9511468c36b557c74ca1
Joel Field <joelmatthewfield@outlook.com>
2021-12-20 11:37:25 +1000

    use react-hook-form

--- src/components/ItemForm/index.tsx
+++ src/components/ItemForm/index.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useEffect, useState } from "react";
-import { Formik } from "formik";
+import { FC, useCallback, useState, useContext } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Category } from "../../models/Category";
 import { Button, Grid, TextField } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import * as Yup from "yup";
-import ImageSelector from "../ImageSelector";
 import API from "../../services/Api";
 
 import Autocomplete, {
@@ -12,7 +12,7 @@ import Autocomplete, {
 } from "@material-ui/lab/Autocomplete";
 import { useMutation, useQueryClient } from "react-query";
 import { CreateItemRequest } from "../../models/Item";
-import { ImageDataPayload } from "../../models/Images";
+import { ImageSelectorContext } from "../ImageSelector/context";
 
 const itemSchema = Yup.object().shape({
   price: Yup.number().required(),
@@ -48,11 +48,10 @@ const ItemForm: FC<{
   categories: Category[];
   initialUrl: string;
   initialName: string;
-  images: ImageDataPayload[];
   onSuccess: (categoryId: number) => void;
-}> = ({ categories, onSuccess, initialName, initialUrl, images }) => {
+}> = ({ categories, onSuccess, initialName, initialUrl }) => {
   const [error, setError] = useState<boolean>(false);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const { selectedImage } = useContext(ImageSelectorContext);
   const queryClient = useQueryClient();
   const initialValues: ItemValues = {
     price: 0,
@@ -61,6 +60,11 @@ const ItemForm: FC<{
     name: initialName,
     image_url: selectedImage,
   };
+  const { register, handleSubmit, control, formState } = useForm<ItemValues>({
+    resolver: yupResolver(itemSchema),
+    defaultValues: initialValues,
+    mode: "onChange",
+  });
 
   const categoryOptions: CategoryOptionType[] = categories.map((category) => ({
     name: category.name,
@@ -76,187 +80,141 @@ const ItemForm: FC<{
     API.post(`/api/categories/${itemReq.category_id}/items`, itemReq)
   );
 
-  useEffect(() => {
-    if (!selectedImage) {
-      document.documentElement.style.width = "800px";
-      document.documentElement.style.height = "600px";
-    } else {
-      document.documentElement.style.width = "400px";
-      document.documentElement.style.height = "400px";
+  const onSubmit = useCallback(async (values) => {
+    if (!values.category || !values.image_url) {
+      return;
     }
-    return () => {
-      document.documentElement.style.width = "400px";
-      document.documentElement.style.height = "400px";
-    };
-  }, [selectedImage]);
 
-  if (!selectedImage) {
-    return (
-      <ImageSelector images={images} setSelectedImage={setSelectedImage} />
-    );
-  }
+    let category_id: number;
+
+    if (!values.category.id) {
+      const category = await createCategory.mutateAsync({
+        name: values.category.name,
+        image_url: values.image_url,
+      });
+      category_id = category.data.id;
+    } else {
+      category_id = values.category.id;
+    }
+
+    await createItem
+      .mutateAsync({
+        name: values.name,
+        category_id,
+        url: values.url,
+        price: values.price,
+        image_url: values.image_url,
+      })
+      .then(() => queryClient.invalidateQueries("categories"))
+      .then(() => {
+        onSuccess(category_id);
+      })
+      .catch(() => {
+        setError(true);
+      });
+  }, []);
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={itemSchema}
-      onSubmit={async (values, { setSubmitting }) => {
-        if (!values.category || !values.image_url) {
-          return;
-        }
-        setSubmitting(true);
-        setError(false);
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3} direction="column">
+        <Grid item xs={12}>
+          <TextField
+            label="Price"
+            fullWidth
+            type="number"
+            {...register("price")}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField fullWidth label="Name" {...register("name")} type="text" />
+        </Grid>
+        <Grid item xs={12}>
+          <Controller
+            name="category"
+            control={control}
+            render={({ field }) => (
+              <Autocomplete
+                value={field.value}
+                onChange={(event: any, newValue: any) => {
+                  if (typeof newValue === "string") {
+                    field.onChange({
+                      ...field.value,
+                      name: newValue,
+                    });
+                  } else if (newValue && newValue.inputValue) {
+                    // Create a new value from the user input
+                    field.onChange({
+                      ...field.value,
+                      name: newValue.inputValue,
+                    });
+                  } else if (newValue && newValue.name && newValue.id) {
+                    field.onChange({
+                      name: newValue.name,
+                      id: newValue.id,
+                    });
+                  } else {
+                    field.onChange(null);
+                  }
+                }}
+                filterOptions={(options, params) => {
+                  const filtered = filter(options, params);
 
-        let category_id: number;
+                  // Suggest the creation of a new value
+                  if (params.inputValue !== "") {
+                    filtered.push({
+                      inputValue: params.inputValue,
+                      name: `Add "${params.inputValue}"`,
+                    });
+                  }
 
-        if (!values.category.id) {
-          const category = await createCategory.mutateAsync({
-            name: values.category.name,
-            image_url: values.image_url,
-          });
-          category_id = category.data.id;
-        } else {
-          category_id = values.category.id;
-        }
+                  return filtered;
+                }}
+                selectOnFocus
+                id="free-solo-with-text-demo"
+                options={categoryOptions}
+                getOptionLabel={(option) => {
+                  // Value selected with enter, right from the input
+                  if (typeof option === "string") {
+                    return option;
+                  }
+                  // Add "xxx" option created dynamically
+                  if (option.inputValue) {
+                    return option.inputValue;
+                  }
+                  // Regular option
+                  return option.name;
+                }}
+                renderOption={(option) => option.name}
+                style={{ width: 300 }}
+                freeSolo
+                renderInput={(params) => (
+                  <TextField {...params} label="Category" variant="outlined" />
+                )}
+              />
+            )}
+          />
+        </Grid>
 
-        await createItem
-          .mutateAsync({
-            name: values.name,
-            category_id,
-            url: values.url,
-            price: values.price,
-            image_url: values.image_url,
-          })
-          .then(() => queryClient.invalidateQueries("categories"))
-          .then(() => {
-            onSuccess(category_id);
-          })
-          .catch(() => {
-            setError(true);
-            setSubmitting(false);
-          });
-      }}
-    >
-      {({
-        values,
-        handleChange,
-        isValid,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        setFieldValue,
-      }) => {
-        return (
-          <form onSubmit={handleSubmit}>
-            <Grid container spacing={3} direction="column">
-              <Grid item xs={12}>
-                <TextField
-                  label="Price"
-                  fullWidth
-                  name="price"
-                  type="number"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.price}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  label="Name"
-                  name="name"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.name}
-                  type="text"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Autocomplete
-                  value={values.category}
-                  onChange={(event: any, newValue: any) => {
-                    if (typeof newValue === "string") {
-                      setFieldValue("category", {
-                        name: newValue,
-                      });
-                    } else if (newValue && newValue.inputValue) {
-                      // Create a new value from the user input
-                      setFieldValue("category", {
-                        name: newValue.inputValue,
-                      });
-                    } else if (newValue && newValue.name && newValue.id) {
-                      setFieldValue("category", {
-                        name: newValue.name,
-                        id: newValue.id,
-                      });
-                    } else {
-                      setFieldValue("category", null);
-                    }
-                  }}
-                  filterOptions={(options, params) => {
-                    const filtered = filter(options, params);
+        <Grid item xs={12}>
+          <Button
+            disabled={formState.isSubmitting || !formState.isValid}
+            variant="contained"
+            type="submit"
+            color="primary"
+          >
+            Add to Robe
+          </Button>
+        </Grid>
 
-                    // Suggest the creation of a new value
-                    if (params.inputValue !== "") {
-                      filtered.push({
-                        inputValue: params.inputValue,
-                        name: `Add "${params.inputValue}"`,
-                      });
-                    }
-
-                    return filtered;
-                  }}
-                  selectOnFocus
-                  id="free-solo-with-text-demo"
-                  options={categoryOptions}
-                  getOptionLabel={(option) => {
-                    // Value selected with enter, right from the input
-                    if (typeof option === "string") {
-                      return option;
-                    }
-                    // Add "xxx" option created dynamically
-                    if (option.inputValue) {
-                      return option.inputValue;
-                    }
-                    // Regular option
-                    return option.name;
-                  }}
-                  renderOption={(option) => option.name}
-                  style={{ width: 300 }}
-                  freeSolo
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Category"
-                      variant="outlined"
-                    />
-                  )}
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Button
-                  disabled={isSubmitting || !isValid || !values.category}
-                  variant="contained"
-                  type="submit"
-                  color="primary"
-                >
-                  Add to Robe
-                </Button>
-              </Grid>
-
-              {!isSubmitting && error && (
-                <Grid item xs={12}>
-                  <Alert severity="error">
-                    Error adding item - please check your inputs
-                  </Alert>
-                </Grid>
-              )}
-            </Grid>
-          </form>
-        );
-      }}
-    </Formik>
+        {/* {!isSubmitting && error && (
+          <Grid item xs={12}>
+            <Alert severity="error">
+              Error adding item - please check your inputs
+            </Alert>
+          </Grid>
+        )} */}
+      </Grid>
+    </form>
   );
 };
 
