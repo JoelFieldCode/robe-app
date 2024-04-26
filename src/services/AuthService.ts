120b5aede672d2c80239e3471781e6c5287ee133
Joel Field <joelmatthewfield@outlook.com>
2024-04-27 08:16:33 +1000

    revert auth service change

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -11,7 +11,7 @@ class AuthService {
           });
         });
       } else {
-        return resolve('ya29.a0Ad52N38nkCiqo4TUCPEwkTbSUYo0BJYmoBs4zEK1hmJ-x_VopJ8B7cxHjVaQSoFEgcbiBAxRvdqVkKlbDGhp2n8n01qkYfABEdqK6_dhO9ZWVdAeIz1lBYcBVBDnWgOwx8mHniJ5loGDWe5QqOqSMouHtn8CfSUJLA4aCgYKAXESARMSFQHGX2Mi_rTxnCrmqxd4EyyeZ77GDQ0170')
+        return resolve('test')
       }
     });
   }
