d1108f6cc52c33d4fa648c0b59163f5bdd39cbdb
Joel Field <joelmatthewfield@outlook.com>
2024-04-27 12:16:54 +1000

    remove unused button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { ChevronLeft, Home, HomeIcon, Plus } from "lucide-react";
+import { Home, Plus } from "lucide-react";
 import { Button } from "../../@/components/ui/button";
 import { Link } from "react-router-dom";
 
 const Header = ({ withAddButton = true }: { withAddButton?: boolean }) => {
   return (
-    <div className="pt-2 flex flex-row items-center justify-between px-2">
+    <div className="pt-2 px-2 flex flex-row items-center justify-between">
       <div>
         <Button asChild size="icon" variant="ghost">
           <Link to="/">
@@ -13,16 +13,7 @@ const Header = ({ withAddButton = true }: { withAddButton?: boolean }) => {
           </Link>
         </Button>
       </div>
-      <div>
-        {/* <Button
-          onClick={() => {
-            // TODO
-          }}
-          variant="ghost"
-        >
-          Robe
-        </Button> */}
-      </div>
+      <div></div>
       {withAddButton && (
         <div>
           <Button asChild size="icon" variant="ghost">
