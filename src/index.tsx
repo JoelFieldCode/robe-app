fcc78e001eaaefd4b277ec3d720481e40f98f288
Joel Field <joelmatthewfield@outlook.com>
2024-04-29 19:55:30 +1000

    remove service worker file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from "react";
-import * as serviceWorker from "./serviceWorker";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -95,8 +94,3 @@ createRoot(document.getElementById("app")!).render(
     </SuperTokensWrapper>
   </QueryClientProvider>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
