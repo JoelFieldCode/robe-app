f893b201f0f3df807627b88002a2f5b4632d2b19
Joel Field <joelmatthewfield@outlook.com>
2024-05-05 08:00:46 +1000

    better empty categories state

--- src/components/CategoriesList/index.tsx
+++ src/components/CategoriesList/index.tsx
@@ -4,6 +4,8 @@ import { client } from "../../services/GraphQLClient";
 import CategoryCard from "../CategoryCard";
 import { FullScreenLoader } from "../FullScreenLoader/FullScreenLoader";
 import { getCategoriesQueryDocument } from "../../queries/getCategoriesQueryDocument";
+import { Button } from "../../@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const CategoriesList: React.FC<{}> = () => {
   const categoriesQuery = useQuery(["categories"], async () =>
@@ -16,15 +18,21 @@ const CategoriesList: React.FC<{}> = () => {
 
   const categories = categoriesQuery.data?.getCategories;
 
-  if (!categories) {
-    return <>You have no categories yet</>;
-  }
-
   return (
     <>
       <h3 className="mb-3 text-lg font-bold">Your categories</h3>
+      {!categories?.length && (
+        <div className="flex flex-col gap-2">
+          <div>Not seeing anything here? Create an item to get started!</div>
+          <div>
+            <Button asChild>
+              <Link to="/items/create">Create Item</Link>
+            </Button>
+          </div>
+        </div>
+      )}
       <div className="flex flex-col gap-4">
-        {categories.map((category) => (
+        {categories?.map((category) => (
           <CategoryCard key={category.id} category={category} />
         ))}
       </div>
