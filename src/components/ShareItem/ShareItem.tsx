462786590daafa5abf659be528fea13f5c5b7891
Joel Field <joelmatthewfield@outlook.com>
2024-05-05 07:28:03 +1000

    use correct params

--- src/components/ShareItem/ShareItem.tsx
+++ src/components/ShareItem/ShareItem.tsx
@@ -5,17 +5,29 @@ import { FullScreenLoader } from "../FullScreenLoader/FullScreenLoader";
 export const ShareItem = () => {
   const navigate = useNavigate();
   const [params] = useSearchParams();
+  /*
+    URL doesn't always come back
+    Text is sometimes the URL? Maybe we should check with JS to see if it's a URL?
+    The title is always correct though
+  */
   const title = params.get("title");
   const text = params.get("text");
   const url = params.get("url");
+  const urlToUse = text ?? url ?? null;
 
-  //   useEffect(() => {
-  //     navigate(`/items/create?name=${name}&url=${url}`, { replace: true });
-  //   }, []);
+  useEffect(() => {
+    navigate(`/items/create?name=${title}&url=${urlToUse}`, { replace: true });
+  }, []);
 
-  return (
-    <div className="break-words">
-      title:{title}, text:{text} url:{url}
-    </div>
-  );
+  if (!urlToUse) {
+    <>Sorry something went wrong</>;
+  }
+
+  return <FullScreenLoader />;
+
+  //   return (
+  //     <div className="break-words">
+  //       title:{title}, text:{text} url:{url}
+  //     </div>
+  //   );
 };
