22842537d8e6e6aef623ae39c25625fd2c78b86a
Joel Field <joelmatthewfield@outlook.com>
2024-05-06 21:59:05 +1000

    legacy GET param approach

--- src/components/ShareItem/ShareItem.tsx
+++ src/components/ShareItem/ShareItem.tsx
@@ -1,9 +1,10 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { FullScreenLoader } from "../FullScreenLoader/FullScreenLoader";
+import { useShareImageStore } from "../../store/shareImageStore";
 
 /*
-  Don't want to redirect twice.. maybe we just move this logic to WithDefaultParams?
+  Legacy GET param way. Not sure if this will run if the user shares link directly?
 */
 export const ShareItem = () => {
   const navigate = useNavigate();
@@ -13,34 +14,20 @@ export const ShareItem = () => {
     Text is sometimes the URL? Maybe we should check with JS to see if it's a URL?
     The title is always correct though
   */
-  // const [image, setImage] = useState(null);
   const title = params.get("title");
   const text = params.get("text");
   const url = params.get("url");
   const urlToUse = text ?? url ?? null;
 
-  // console.log(window.sessionStorage.getItem("image"));
-
-  // navigator.serviceWorker.onmessage = function (event) {
-  //   console.log({ event });
-  //   const imageBlob = event.data.file;
-  //   console.log({ imageBlob });
-  //   setImage(imageBlob);
-  //   // we now have the file data and can for example use it as a source for an img with the id image on our page
-  //   // const image = document.getElementById("image");
-  //   // image.src = URL.createObjectURL(imageBlob);
-  // };
-
   useEffect(() => {
     // TODO handle if title/URL is null. Shoudn't pass null in the URL..
+    useShareImageStore.setState({
+      title,
+      text,
+      url,
+    });
     navigate(`/items/create?name=${title}&url=${urlToUse}`, { replace: true });
   }, []);
 
   return <FullScreenLoader />;
-
-  //   return (
-  //     <div className="break-words">
-  //       title:{title}, text:{text} url:{url}
-  //     </div>
-  //   );
 };
