9d30ac80fc8e7079b17e477863fa7f1d405bf90f
Joel Field <joelmatthewfield@outlook.com>
2024-05-10 21:25:43 +1000

    make sure active tab is highlighted

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,29 @@
 import { Home, Plus, User } from "lucide-react";
 import React, { ReactNode, isValidElement, cloneElement } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { cn } from "../../@/lib/utils";
+
+const links: { name: string; href: string; icon: ReactNode }[] = [
+  {
+    name: "Home",
+    href: "/",
+    icon: <Home />,
+  },
+  {
+    name: "Create",
+    href: "/items/create",
+    icon: <Plus />,
+  },
+  {
+    name: "Profile",
+    href: "/profile",
+    icon: <User />,
+  },
+];
 
 export const Footer = () => {
   const navigate = useNavigate();
-  const links: { name: string; href: string; icon: ReactNode }[] = [
-    {
-      name: "Home",
-      href: "/",
-      icon: <Home />,
-    },
-    {
-      name: "Create",
-      href: "/items/create",
-      icon: <Plus />,
-    },
-    {
-      name: "Profile",
-      href: "/profile",
-      icon: <User />,
-    },
-  ];
+  const { pathname } = useLocation();
 
   return (
     <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
@@ -28,18 +31,29 @@ export const Footer = () => {
         {links.map(({ href, icon, name }) => {
           return (
             <button
-              type="button"
               key={name}
+              type="button"
               onClick={() => navigate(href)}
               className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
             >
               {isValidElement(icon) &&
                 cloneElement<any>(icon, {
-                  className:
+                  className: cn(
                     "w-7 h-7 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500",
+                    {
+                      "text-blue-600": pathname === href,
+                    }
+                  ),
                   "aria-hidden": "true",
                 })}
-              <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
+              <span
+                className={cn(
+                  "text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500",
+                  {
+                    "text-blue-600": pathname === href,
+                  }
+                )}
+              >
                 {name}
               </span>
             </button>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Home, Plus, User } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Button } from "../../@/components/ui/button";
 import { Link } from "react-router-dom";
 
